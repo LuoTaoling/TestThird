ec4f6d119c88cd2afcaa415a2738fc4dd8da5803
ltl <3132170072@qq.com>
2024-12-19 11:31:16 +0800

    11

---
+++ src/main/java/com/example/testthird/TestThirdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testthird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestThirdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testthird/TestThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThirdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
